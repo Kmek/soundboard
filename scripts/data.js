e4b668a8c7cef16140848fa30a2294bf4683d71b
Kmek <kmekeefe@gmail.com>
2025-09-04 22:54:20 -0400

    Add more audio files

--- scripts/data.js
+++ scripts/data.js
@@ -10,8 +10,148 @@ const audioData = [
     cat: "Mooncake"
   },
   {
-    title: "meew", 
+    title: "meeOw", 
     file: "audio3.mp3",
     cat: "Mooncake"
-  }
+  },
+  {
+    title: "mrrr", 
+    file: "audio4.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "meeer", 
+    file: "audio5.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "mrrr", 
+    file: "audio6.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "mekak", 
+    file: "audio7.mp3",
+    cat: "Void"
+  },
+  {
+    title: "Mrrr", 
+    file: "audio8.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "mrrROw", 
+    file: "audio9.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "meeEew", 
+    file: "audio10.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "meeEEow", 
+    file: "audio11.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "mr mrt", 
+    file: "audio12.mp3",
+    cat: "Void"
+  },
+  {
+    title: "meOw", 
+    file: "audio13.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "mEOOOw", 
+    file: "audio14.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "mrt m mr", 
+    file: "audio15.mp3",
+    cat: "Void"
+  },
+  {
+    title: "meo ow", 
+    file: "audio16.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "mmrow", 
+    file: "audio17.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "m maaaow", 
+    file: "audio18.mp3",
+    cat: "Void"
+  },
+  {
+    title: "mEw", 
+    file: "audio19.mp3",
+    cat: "Void"
+  },
+  {
+    title: "prrrrrrrrr...", 
+    file: "audio20.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "mEw", 
+    file: "audio21.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "mee m Ew", 
+    file: "audio22.mp3",
+    cat: "Void"
+  },
+  {
+    title: "mroow mr", 
+    file: "audio23.mp3",
+    cat: "Void"
+  },
+  {
+    title: "mROw", 
+    file: "audio24.mp3",
+    cat: "Void"
+  },
+  {
+    title: "moow meew", 
+    file: "audio25.mp3",
+    cat: "Void"
+  },
+  {
+    title: "mr-row", 
+    file: "audio26.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "e", 
+    file: "audio27.mp3",
+    cat: "Void"
+  },
+  {
+    title: "mrrrow", 
+    file: "audio28.mp3",
+    cat: "Mooncake"
+  },
+  {
+    title: "ek err", 
+    file: "audio29.mp3",
+    cat: "Void"
+  },
+  {
+    title: "meew mr", 
+    file: "audio30.mp3",
+    cat: "Void"
+  },
+  {
+    title: "mo mrrow", 
+    file: "audio31.mp3",
+    cat: "Void"
+  },
 ]
